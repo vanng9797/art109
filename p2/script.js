92d8f644f53976a4af44c6931752687c7225631c
vanng9797 <vanng9797@gmail.com>
2021-10-25 01:36:42 -0700

    bokeh lights

--- p2/script.js
+++ p2/script.js
@@ -3,11 +3,26 @@ var parallaxInstance = new Parallax(scene, {
   relativeInput: true
 });
 
-$(document).ready(function(){
-    $("layer3").animate({
-      left: '250px',
-      opacity: '0.5',
-      height: '150px',
-      width: '150px'
-    });
-  });
+$(document).ready(function() {
+    $(".layer3").animate({
+        left: '250px',
+        opacity: '0.5',
+        height: '50%',
+        width: '50%'
+      },
+      5000, 'swing');
+  $('.layer3').fadeOut(5000);
+  function loop(){
+     $('.layer9')
+       .animate({top:12},1000)
+       .animate({top:9},1000, loop);
+     $('.layer8')
+       .animate({left:14},1000)
+       .animate({left:10},1000, loop);
+     $('.layer7')
+       .animate({right:16},1000)
+       .animate({right:12},1000, loop);
+    }
+
+    loop(); // call this wherever you want
+});
