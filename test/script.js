208162cc2668e03ba8b416615f23359a941f7607
vanng9797 <vanng9797@gmail.com>
2021-12-11 16:16:09 -0800

    trying lottie

---
+++ test/script.js
@@ -0,0 +1,8 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById('anim'), // Required
+  path: 'leaves.json', // Required
+  renderer: 'svg', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: "leaves", // Name for future reference. Optional.
+})
